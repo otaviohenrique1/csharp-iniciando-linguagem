3049530d6fcedcdc0a4cdfa9f2ce6bfe1f4ee1d0
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-12-22 18:38:18 -0300

    Commit

---
+++ aulas/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld;
+using System;
+
+class Aula2
+{
+  // static void Main(string[] args)
+  // {
+  //   Console.WriteLine("Ola mundo!");
+  //   Console.WriteLine("Tecla enter");
+  //   Console.ReadLine();
+  // }
+}
